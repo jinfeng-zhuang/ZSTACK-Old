e601c5bdce38a8525af565488a75d9713249d2ff
Jinfeng Zhuang <Jinfengz@v-silicon.com>
2021-01-07 18:53:38 +0800

    new project: test_cjson
    Signed-off-by: Jinfeng Zhuang <Jinfengz@v-silicon.com>

---
+++ Application/test_cjson/main.cpp
@@ -0,0 +1,73 @@
+#include <zstack.h>
+#include <3rdparty/cJSON.h>
+
+#include "main.h"
+
+struct application app;
+
+/* return 1 if the monitor supports full hd, 0 otherwise */
+int supports_full_hd(const char * const monitor)
+{
+    const cJSON *resolution = NULL;
+    const cJSON *resolutions = NULL;
+    const cJSON *name = NULL;
+    int status = 0;
+    cJSON *monitor_json;
+    
+    monitor_json = cJSON_Parse(monitor);
+    if (monitor_json == NULL)
+    {
+        const char *error_ptr = cJSON_GetErrorPtr();
+        if (error_ptr != NULL)
+        {
+            fprintf(stderr, "Error before: %s\n", error_ptr);
+        }
+        status = 0;
+        goto end;
+    }
+
+    name = cJSON_GetObjectItemCaseSensitive(monitor_json, "name");
+    if (cJSON_IsString(name) && (name->valuestring != NULL))
+    {
+        printf("Checking monitor \"%s\"\n", name->valuestring);
+    }
+
+    resolutions = cJSON_GetObjectItemCaseSensitive(monitor_json, "resolutions");
+    cJSON_ArrayForEach(resolution, resolutions)
+    {
+        cJSON *width = cJSON_GetObjectItemCaseSensitive(resolution, "width");
+        cJSON *height = cJSON_GetObjectItemCaseSensitive(resolution, "height");
+
+        if (!cJSON_IsNumber(width) || !cJSON_IsNumber(height))
+        {
+            status = 0;
+            goto end;
+        }
+
+        printf("%dx%f\n", width->valueint, height->valuedouble);
+    }
+
+end:
+    cJSON_Delete(monitor_json);
+    return status;
+}
+
+int main(int argc, char *argv[])
+{
+    int ret;
+
+    if (param_parser(argc, argv, &app) == -1) {
+        print_usage();
+        return -1;
+    }
+
+    log_init(app.param.log_config);
+
+    app.file_content = file_load(app.param.filename, &app.file_size);
+
+    ret = supports_full_hd((const char *)app.file_content);
+
+    info("supports_full_hd return %d\n", ret);
+
+    return 0;
+}
