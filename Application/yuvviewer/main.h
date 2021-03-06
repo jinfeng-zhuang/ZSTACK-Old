72cabacdce41377e00915e1050a6a8b7d9c8b133
jinfeng <jinfeng.zhuang@aliyun.com>
2021-03-06 11:56:03 +0800

    c to cpp
    Signed-off-by: jinfeng <jinfeng.zhuang@aliyun.com>

---
+++ Application/yuvviewer/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define LOG_CONFIG_LENGTH   (256)
+
+struct application {
+    struct {
+        char log_config[LOG_CONFIG_LENGTH];
+		int format;
+		int width;
+		int height;
+		int stride;
+		char filename[FILENAME_MAX];
+    } param;
+};
+
+#endif
---
+++ Application/yuvviewer/usage.cpp
@@ -0,0 +1,16 @@
+const char *usage =
+"\
+Usage: yuvviewer [IP] [OPTION]... [FILE]...\n\
+\n\
+This is a template project to generate a new one.\n\
+\n\
+  --version     version info\n\
+  --help        this message\n\
+  --log         log config\n\
+\n\
+Examples:\n\
+  yuvviewer --log \"app:5\"\n\
+  yuvviewer --format=I420 --resolution=1920x1080 --stride=6144 test.yuv\n\
+\n\
+Copyright @ 2020, Jinfeng Zhuang\n\
+";
