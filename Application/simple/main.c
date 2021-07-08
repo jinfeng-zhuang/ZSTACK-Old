28dac2843c7982094f3a4ef193a2894ddc32d566
jinfengz <jinfeng.zhuang@aliyun.com>
2021-07-08 10:00:49 +0800

    clean simple project.
    Signed-off-by: jinfengz <jinfeng.zhuang@aliyun.com>

--- Application/simple/main.c
+++ Application/simple/main.c
@@ -1,16 +1,9 @@
 #include <zstack/zstack.h>
 #include <v-silicon/v-silicon.h>
 #include <stdio.h>
+#include <Windows.h>
 
-extern void serial_port_list(void);
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv)
 {
-	log_init("net_connect,dbg_init,main,time,file");
-
-	DEBUG("Hello World\n");
-
-	dbg_init("0.0.0.0");
-
-	serial_port_list();
+	return 0;
 }
