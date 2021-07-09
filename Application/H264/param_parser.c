2f1c5bc33b238867eb2de115ec7fcc50cd5febfa
jinfengz <jinfeng.zhuang@aliyun.com>
2021-07-09 18:36:37 +0800

    add h264 project
    Signed-off-by: jinfengz <jinfeng.zhuang@aliyun.com>

--- Application/H264/param_parser.c
+++ Application/H264/param_parser.c
@@ -1,16 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <zstack.h>
-
-#include <3rdparty/getopt.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <zstack/zstack.h>
+#include <getopt/getopt.h>
 #include "main.h"
-
-#ifdef _WIN32
-#include <Windows.h>
-#else
-#include <time.h>
-#endif
 
 enum {
     OPTION_VERSION = 1,
@@ -28,13 +22,7 @@ static struct option opts[] = {
 int param_parser(int argc, char *argv[], struct application *app)
 {
     int c;
-    
-#ifdef _WIN32
-    SYSTEMTIME time;
-#else
-    time_t seconds;
-    struct tm *tm;
-#endif
+    struct time tm;
 
     if (argc <= 1)
         return -1;
@@ -42,14 +30,13 @@ int param_parser(int argc, char *argv[], struct application *app)
     while((c=getopt_long(argc, argv, "", opts, NULL))!=-1){
         switch (c) {
         case OPTION_VERSION:
-            log_info("Version: %s\n", version);
+            PRINT("Version: %s\n", version);
             return -1;
         case OPTION_HELP:
-            log_info(usage);
             return -1;
         case OPTION_LOG:
             if (strlen(optarg) >= LOG_CONFIG_LENGTH) {
-                log_info("log config out of range (0, %d)\n", LOG_CONFIG_LENGTH);
+                PRINT("log config out of range (0, %d)\n", LOG_CONFIG_LENGTH);
                 return -1;
             }
             strncpy(app->param.log_config, optarg, LOG_CONFIG_LENGTH);
@@ -65,30 +52,20 @@ int param_parser(int argc, char *argv[], struct application *app)
         memcpy(app->param.input_filename, argv[optind], strlen(argv[optind]));
     }
 
-    // Do param validation
+    /*************************************************************************
+     * Do param validation
+     ************************************************************************/
+
     if (0 == app->param.output_filename[0]) {
-#ifdef _WIN32
-        GetLocalTime(&time);
-        _snprintf(app->param.output_filename, FILENAME_MAX, "h264_%d-%02d-%02d_%02d-%02d-%02d.log",
-            time.wYear,
-            time.wMonth,
-            time.wDay,
-            time.wHour,
-            time.wMinute,
-            time.wSecond
-            );
-#else
-        seconds = time(NULL);
-        tm = localtime(&seconds);
-        snprintf(app->param.output_filename, FILENAME_MAX, "h264_%d-%02d-%02d_%02d-%02d-%02d.log",
-            tm->tm_year + 1900,
-            tm->tm_mon + 1,
-            tm->tm_day,
-            tm->tm_hour,
-            tm->tm_min,
-            tm->tm_sec
-            );
-#endif
+        get_time(&tm);
+        snprintf(app->param.output_filename, FILENAME_MAX, "%d-%02d-%02d_%02d-%02d-%02d",
+            tm.year,
+            tm.month,
+            tm.day,
+            tm.hour,
+            tm.min,
+            tm.sec
+        );
     }
 
     return 0;
