9cb073329707c60d72235990963b133c64eb10f8
jinfengz <jinfeng.zhuang@aliyun.com>
2021-07-09 10:50:20 +0800

    update newproj.py, double click to run
    Signed-off-by: jinfengz <jinfeng.zhuang@aliyun.com>

--- Application/newproj.py
+++ Application/newproj.py
@@ -1,13 +1,10 @@
 import sys
 import os
 import shutil
+import time
 
-if (len(sys.argv) < 3):
-    print("Usage: python newproj.py <template project name> <new project name>")
-    exit(0)
-
-template_project_name = sys.argv[1]
-new_project_name = sys.argv[2]
+new_project_name = input("New Proect Name:")
+template_project_name = "template"
 
 print(template_project_name, "=>", new_project_name)
 
@@ -15,14 +12,12 @@ source_path = os.path.abspath(template_project_name)
 target_path = os.path.abspath(new_project_name)
 
 if os.path.exists(target_path):
-    shutil.rmtree(target_path)
-'''
-if not os.path.exists(target_path):
-    os.mkdir(target_path)
-'''
-shutil.copytree(source_path, target_path)
+    #shutil.rmtree(target_path)
+    print("New Project already exist")
+    time.sleep(1)
+    exit(0)
 
-print('Done!')
+shutil.copytree(source_path, target_path)
 
 os.chdir(target_path)
 
@@ -30,9 +25,24 @@ if os.path.exists("Release"):
     shutil.rmtree("Release")
 if os.path.exists("Debug"):
     shutil.rmtree("Debug")
+if os.path.exists("x64"):
+    shutil.rmtree("x64")
 
-filename_vcproj_old = template_project_name + '.vcproj'
-filename_vcproj_new = new_project_name + '.vcproj'
+##########################################################
+filename_vcproj_old = template_project_name + '.vcxproj.filters'
+filename_vcproj_new = new_project_name + '.vcxproj.filters'
+
+os.rename(filename_vcproj_old, filename_vcproj_new)
+
+##########################################################
+filename_vcproj_old = template_project_name + '.vcxproj.user'
+filename_vcproj_new = new_project_name + '.vcxproj.user'
+
+os.rename(filename_vcproj_old, filename_vcproj_new)
+
+##########################################################
+filename_vcproj_old = template_project_name + '.vcxproj'
+filename_vcproj_new = new_project_name + '.vcxproj'
 
 os.rename(filename_vcproj_old, filename_vcproj_new)
 
@@ -49,4 +59,15 @@ for line in file_vcproj_lines:
     file_vcproj_w.write(line)
 file_vcproj_w.close()
 
-os.chdir("..")
+##########################################################
+
+file_r = open("usage.c", "r")
+file_lines = file_r.readlines()
+file_r.close()
+
+file_w = open("usage.c", "w")
+for line in file_lines:
+    if template_project_name in line:
+        line = line.replace(template_project_name, new_project_name)
+    file_w.write(line)
+file_w.close()
